04f24494c45ed43c254fd35e14ae3b16b379658c
Harsh Soni <57137427+24Cipher@users.noreply.github.com>
2021-05-23 21:36:38 +0530

    remove creds

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,8 @@
 import firebase from 'firebase';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAzGUxgSvkmcHfTvGLPNx-PqVuY5wRX3Qo",
-    authDomain: "instagram-clone-7f1f3.firebaseapp.com",
-    databaseURL: "https://instagram-clone-7f1f3.firebaseio.com",
-    projectId: "instagram-clone-7f1f3",
-    storageBucket: "instagram-clone-7f1f3.appspot.com",
-    messagingSenderId: "959247592936",
-    appId: "1:959247592936:web:63f337d6a81423207c085d",
-    measurementId: "G-183D8J2J1E"
-  };
+   // add Firebase config
+};
 
 
 const fbApp = firebase.initializeApp(firebaseConfig)
