865e997b94a6b1d7e527ed508471bd30176b7ad4
Rhythm Bhiwani <47269183+rhythmbhiwani@users.noreply.github.com>
2022-10-11 00:10:50 +0530

    Removed Commented Code

--- src/App.js
+++ src/App.js
@@ -19,28 +19,6 @@ import { HeartIcon, HomeIcon } from "@primer/octicons-react";
 import { ImCompass2 } from "react-icons/im";
 import StickyBox from "react-sticky-box/dist/esnext";
 
-// function getModalStyle() {
-//   const top = 50;
-//   const left = 50;
-
-//   return {
-//     top: `${top}%`,
-//     left: `${left}%`,
-//     transform: `translate(-${top}%, -${left}%)`,
-//   };
-// }
-
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     position: "absolute",
-//     width: 400,
-//     backgroundColor: theme.palette.background.paper,
-//     border: "2px solid #000",
-//     boxShadow: theme.shadows[5],
-//     padding: theme.spacing(2, 4, 3),
-//   },
-// }));
-
 function App() {
   const [posts, setPosts] = useState([]);
   const [upload, setUpload] = useState(false);
